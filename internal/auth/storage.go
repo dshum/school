073450d3751b2c10a20b02222ab105f681b12b66
Denis Shumeev <denis-shumeev@yandex.ru>
2022-01-01 21:14:02 +0300

    ---

--- internal/auth/storage.go
+++ internal/auth/storage.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/dshum/school/internal/user"
 	"github.com/dshum/school/internal/utils"
 	"github.com/jackc/pgx/v4"
@@ -24,34 +23,33 @@ func NewStorage(db *pgx.Conn) Storage {
 }
 
 func (s *authStorage) Attempt(login, password string) (user.User, error) {
-	user := user.User{}
+	u := user.User{}
 
 	if login == "" || password == "" {
-		return user, errors.New("empty login or password")
+		return u, errors.New("empty login or password")
 	}
 
 	row := s.db.QueryRow(context.Background(),
 		"SELECT id, login, password, email, first_name, last_name, super_user, banned, created_at, updated_at FROM admin_users WHERE login = $1", login)
 	err := row.Scan(
-		&user.Id,
-		&user.Login,
-		&user.Password,
-		&user.Email,
-		&user.FirstName,
-		&user.LastName,
-		&user.SuperUser,
-		&user.Banned,
-		&user.CreatedAt,
-		&user.UpdatedAt)
+		&u.Id,
+		&u.Login,
+		&u.Password,
+		&u.Email,
+		&u.FirstName,
+		&u.LastName,
+		&u.SuperUser,
+		&u.Banned,
+		&u.CreatedAt,
+		&u.UpdatedAt)
 
 	if err != nil {
-		fmt.Printf("%t, %v", err, err)
-		return user, errors.New("incorrect login or password")
+		return u, errors.New("incorrect login")
 	}
 
-	if !utils.PasswordVerify(password, user.Password) {
-		return user, errors.New("incorrect login or password")
+	if !utils.PasswordVerify(password, u.Password) {
+		return u, errors.New("incorrect login or password")
 	}
 
-	return user, err
+	return u, err
 }
